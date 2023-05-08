539bf382c74c0584fc62b5da5f2ccffe448f7978
nooberking-Desktop <davidmbmanuel@gmail.com>
2023-05-08 14:12:21 +0200

    Création endpoint for receiving zip filz

---
+++ UnzipperFileAPI/Controllers/UnzipController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UnzipperFileAPI.Controllers
+{
+    [Route("UnzipperApi/[controller]")]
+    [ApiController]
+    public class UnzipController : ControllerBase
+    {
+        private readonly ILogger<UnzipController> _logger;
+
+        public UnzipController(ILogger<UnzipController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveZipFile(IFormFile file)
+        {
+            var filePath = Path.GetTempFileName();
+
+            using (var stream = System.IO.File.Create(filePath))
+            {
+                await file.CopyToAsync(stream);
+            }
+            return Ok();
+        }
+    }
+}
