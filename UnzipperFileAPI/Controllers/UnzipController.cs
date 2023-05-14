f3977e11e12f36f0b415c925c47be51d800e93ce
nooberking-Desktop <davidmbmanuel@gmail.com>
2023-05-14 09:38:01 +0200

    Unzip file on App/temp folder

--- UnzipperFileAPI/Controllers/UnzipController.cs
+++ UnzipperFileAPI/Controllers/UnzipController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using UnzipperFileAPI.Services;
 
 namespace UnzipperFileAPI.Controllers
 {
@@ -9,43 +10,48 @@ namespace UnzipperFileAPI.Controllers
     {
         private readonly ILogger<UnzipController> _logger;
 
+        private readonly IUnzipService _unzipService; 
+
         private readonly string _destinationFolder = "/media/source/";
 
-        public UnzipController(ILogger<UnzipController> logger)
+        public UnzipController(ILogger<UnzipController> logger, IUnzipService unzipService)
         {
             _logger = logger;
+            _unzipService = unzipService;
         }
 
         [HttpPost]
         public async Task<IActionResult> SaveZipFile(IFormFile file)
         {
-            try
-            {
-                string fileName = HttpContext.Request.Headers.ContainsKey("FileTitle") ? HttpContext.Request.Headers["FileTitle"] : global::UnzipperFileAPI.Controllers.UnzipController.GetRandomizedName();
+       
+            string fileName = HttpContext.Request.Headers.ContainsKey("FileTitle") ? HttpContext.Request.Headers["FileTitle"] : GetRandomizedName(Path.GetTempFileName());
 
-                string destFile = GetDestinationFilePath(fileName);
+            string destFile = GetDestinationFilePath(fileName);
 
-                using (var stream = System.IO.File.Create(destFile))
-                {
-                    await file.CopyToAsync(stream);
-                }
-                return Ok();
-            }
-            catch (Exception e)
+            using (var stream = System.IO.File.Create(destFile))
             {
-                return BadRequest(e);
+                Console.WriteLine(destFile);
+                await file.CopyToAsync(stream);
             }
+            return Ok();
+           
             
         }
-
-        private static string GetRandomizedName()
+        private string GetRandomizedName(string tempFileName)
         {
-            return Path.GetTempFileName().Split("/tmp/")[1].Replace(".tmp", "");
+            return tempFileName.Split("/tmp/")[1].Replace(".tmp", "");
         }
 
         private string GetDestinationFilePath (string fileName)
         {
             return $"{_destinationFolder}{fileName}.zip";
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UnzipFile(string destFile)
+        {
+            await _unzipService.UnzipFile(destFile);
+            return Ok();
         }
     }
 }
---
+++ UnzipperFileAPI/Services/UnzipService.cs
@@ -0,0 +1,14 @@
+﻿using System.IO.Compression; 
+
+namespace UnzipperFileAPI.Services
+{
+    public class UnzipService : IUnzipService
+    {
+      
+        public Task UnzipFile(string zipPath)
+        {
+            ZipFile.ExtractToDirectory(zipPath, @"./temp", true);
+            return Task.CompletedTask;
+        }
+    }
+}
