aae0b354d17d6f7fe0e806035019bb6fd0f5cabe
nooberking-Desktop <davidmbmanuel@gmail.com>
2023-05-14 10:06:13 +0200

    [Refacto] : delagating SaveZip file from controller to Service

--- UnzipperFileAPI/Services/IUnzipService.cs
+++ UnzipperFileAPI/Services/IUnzipService.cs
@@ -2,6 +2,10 @@
 {
     public interface IUnzipService
     {
+        Task SaveZipFile(string fileName, IFormFile file);
         Task UnzipFile(string path);
+
+        string ExtractRandomizedName(string tempFileName);
+
     }
 }
